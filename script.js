ca63af4ffccb7355d431387831d6000d05dce722
asad200-OK <asadullakhan0098@gmail.com>
2024-01-30 01:45:32 +0530

    Add functionality for displaying numbers

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 const screen = document.querySelector('.screen');
+
 const allClearBtn = document.querySelector('.ac');
+
 const deleteBtn = document.querySelector('.del');
+
 const operatorBtns = document.querySelectorAll('.operator');
+
 const numberBtns = document.querySelectorAll('.number');
+
 const decimalBtn = document.querySelector('.point');
 
+let num1 = '';
+
+let op = null;
+
+let num2 = '';
+
+let displayValue;
+
+numberBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        let number = e.target.textContent;
+        op === null ? num1 += number : 
+        num2 += number;
+        num2 ? displayValue = num2 :
+        displayValue = num1;
+        display(displayValue); 
+    })
+})
 
 function add(x,y) {
     return x + y;
@@ -28,4 +51,8 @@ function pow(x,y) {
 
 function getRemainder(x,y) {
     return x % y;
+}
+
+function display(value) {
+    screen.textContent = value;
 }
